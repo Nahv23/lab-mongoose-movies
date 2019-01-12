df4972042e01be8ebdc793d17932cc67bbcf4ef7
Antonio Colmenares <colmena.anto@gmail.com>
2019-01-12 10:10:53 -0500

    formulalio y lista listos

--- controllers/celebrities.controller.js
+++ controllers/celebrities.controller.js
@@ -1,11 +1,24 @@
 const Celebrity = require('../models/celebrity.js');
 
-module.exports.list = (req, res, next) => {
+module.exports.form = (req, res, next) => {
   Celebrity.find()
     .then ((celebrities) => res.render ('celebrities/index', {celebrities}))
     .catch(err => next(err))
 };
 
+module.exports.list = (req, res, next) => {
+  Celebrity.find()
+    .then ((celebrities) => res.render ('celebrities/list', {celebrities}))
+    .catch(err => next(err))
+};
+
 module.exports.create = (req, res, next) => {
   res.render('celebrities/create');
+}
+
+module.exports.doCreate = (req, res, next) => {
+  const celebrity = new Celebrity(req.body);
+
+  celebrity.save()
+    .then((celebrity) => { res.redirect('/celebrities/list' )});
 }
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,11 +4,10 @@ const router = express.Router();
 
 const celebritiesController = require('../controllers/celebrities.controller');
 
-router.get('/', celebritiesController.list);
-
+router.get('/', celebritiesController.form);
+router.get('/list', celebritiesController.list);
 router.get('/create', celebritiesController.create)
-
-// router.post('/create', usersController.doCreate)
+router.post('/create', celebritiesController.doCreate)
 
 // router.get('/:id', usersController.get)
 // router.post('/:id/delete', usersController.delete)
