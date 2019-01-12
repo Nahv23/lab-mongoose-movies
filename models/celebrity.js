13867cc75f751ef6a01f00d8e0240526fb66fdad
Antonio Colmenares <colmena.anto@gmail.com>
2019-01-12 08:49:39 -0500

    iteration 1 done

---
+++ models/celebrity.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    catchPhrase: {
+        type: String,
+        required: true,
+    }
+  }, {
+    timestamps: true
+  });
+  
+  module.exports = mongoose.model('Celebrity', schema);
